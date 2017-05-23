10c7df58d560482739a0db8fcabdea03e9739e27
Günce BEKTAŞ <guncebektas@gmail.com>
2017-05-23 09:43:37 +0300

    update

--- resources/views/app/posts/show.blade.php
+++ resources/views/app/posts/show.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 @section('content')
 
-<header class="intro-header" style="background-image: url({{$post->image}})">
+<header class="intro-header" style="background-image: url(../../data/_images/{!!$post->image!!})">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                 <div class="post-heading">
-                    <h1>{{$post->title}}</h1>
-                    <h2 class="subheading">{{$post->summary}}</h2>
+                    <h1>{!! $post->title !!}</h1>
+                    <h2 class="subheading">{!! $post->summary !!}</h2>
                     <span class="meta">Posted by {{$post->user->name}} {{$post->published_at}}</span>
                 </div>
             </div>
@@ -20,10 +20,10 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                {{$post->text}}
+                {!! $post->text !!}
             </div>
         </div>
     </div>
 </article>
-
+ 
 @endsection
